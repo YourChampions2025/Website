e68de7ad47f8ba2edeb6d60a8f991dadd734ba0e
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-10-27 20:01:02 -0300

    feat:Development of FAQ Component and Integration into Home Section, as well as Creation of Collapse Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ContactUs from "@/components/globals/contactUs/contact-us";
 import Header from "@/components/globals/header/header";
+import FrequentQuestions from "@/components/screens/home/FAQ/frequent-questions";
 import HomeAboutUs from "@/components/screens/home/home-about-us/home-about-us";
 import HomeBook from "@/components/screens/home/home-book/home-book";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <HomeAboutUs />
       <HomeBook />
+      <FrequentQuestions />
       <ContactUs />
     </div>
   );
--- components/screens/home/FAQ/frequent-questions.tsx
+++ components/screens/home/FAQ/frequent-questions.tsx
@@ -1,7 +1,88 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import classNames from "classnames";
+import React, { useState } from "react";
+import styles from "./frequent-questions.module.css";
+import Collapse from "@/components/globals/collapse/collapse";
+import { IoIosArrowDropdown, IoIosArrowDropup } from "react-icons/io";
+import faqImage from "@/public/images/faq.png";
+
+const questionsAndAnswers = [
+  {
+    question:
+      "What do I need to prove to be successful in my personal injury case?",
+    answer:
+      "You will file an insurance claim with your own auto insurance company after your car accident. Florida is a fault state for car accidents, so you are required to carry personal injury protection (PIP) coverage as part of your auto insurance policy. This coverage means your insurer has an obligation to cover certain losses when you are involved in a collision. But, insurers are rarely fair when it comes to victim compensation.",
+  },
+  {
+    question: "How much is my claim worth?",
+    answer:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+  },
+  {
+    question: "Do I need to hire a personal injury lawyer?",
+    answer:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+  },
+  {
+    question: "How much does a personal injury lawyer cost?",
+    answer:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+  },
+];
 
 function FrequentQuestions() {
-  return <div>FrequentQuestions</div>;
+  const [selectedItem, setSelectedItem] = useState<number | null>(0);
+
+  const toggleVisibility = (i: number) => {
+    setSelectedItem((prevItem) => (prevItem === i ? null : i));
+  };
+
+  return (
+    <section className={styles.sectionContainer}>
+      <div className={styles.contentContainer}>
+        <h2 className={styles.heading}>frequently asked questions</h2>
+        <div className={styles.content}>
+          <div className={styles.questionsContainer}>
+            {questionsAndAnswers.map((item, i) => {
+              const { question, answer } = item;
+              const isOpen = selectedItem === i;
+
+              return (
+                <div
+                  key={i}
+                  className={classNames(
+                    styles.questionItem,
+                    isOpen ? styles.questionItemOpen : null
+                  )}
+                >
+                  <div
+                    onClick={() => toggleVisibility(i)}
+                    className={classNames(
+                      styles.questionHeader,
+                      isOpen ? styles.questionOpen : styles.questionClosed
+                    )}
+                  >
+                    <h3 className={styles.questionText}>{question}</h3>
+                    <span className={styles.iconContainer}>
+                      {isOpen ? <IoIosArrowDropup /> : <IoIosArrowDropdown />}
+                    </span>
+                  </div>
+
+                  <Collapse isOpen={isOpen}>
+                    <p className={styles.answerText}>{answer}</p>
+                  </Collapse>
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.mediaContainer}>
+            <Image alt="" src={faqImage} className={styles.faqMedia} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default FrequentQuestions;
