5c363488104a268d8fb0eaa24ecc02efa44d006e
Davin Casely <24211985+DCasely@users.noreply.github.com>
2024-11-21 00:06:34 -0500

    pulse button for sound

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-instrument-sans)"],
-        serif: ["var(--font-instrument-serif)"],
+        sans: ['var(--font-instrument-sans)'],
+        serif: ['var(--font-instrument-serif)'],
       },
     },
     screens: {
-      xs: "480px",
-      sm: "640px",
-      sd: "720px",
-      md: "900px",
-      lg: "1080px",
-      xl: "1240px",
-      "2xl": "1440px",
+      xs: '480px',
+      sm: '640px',
+      sd: '720px',
+      md: '900px',
+      lg: '1080px',
+      xl: '1240px',
+      '2xl': '1440px',
     },
   },
   plugins: [],
