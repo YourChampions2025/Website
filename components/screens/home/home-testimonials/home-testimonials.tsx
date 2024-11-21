38fb77615364fee7f6c34b6f9b169fa5a32b570c
Davin Casely <24211985+DCasely@users.noreply.github.com>
2024-11-21 00:25:23 -0500

    added animation to slider testimonials

--- components/screens/home/home-testimonials/home-testimonials.tsx
+++ components/screens/home/home-testimonials/home-testimonials.tsx
@@ -1,13 +1,14 @@
-"use client";
+'use client';
 
-import React from "react";
-import { FaStar } from "react-icons/fa6";
-import styles from "./home-testimonials.module.css";
-import CustomButton from "@/components/globals/forms/custom-button/custom-button";
-import Link from "next/link";
-import { TestimonialProps } from "@/types/types";
-import PortableTextComponent from "@/components/globals/general/portable-text-component/portable-text-component";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React from 'react';
+import { FaStar } from 'react-icons/fa6';
+import styles from './home-testimonials.module.css';
+import CustomButton from '@/components/globals/forms/custom-button/custom-button';
+import Link from 'next/link';
+import { TestimonialProps } from '@/types/types';
+import PortableTextComponent from '@/components/globals/general/portable-text-component/portable-text-component';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 
 interface HomeTestimonialsProps {
   testimonials: TestimonialProps[];
@@ -19,8 +20,8 @@ function HomeTestimonials({ testimonials }: HomeTestimonialsProps) {
       <div className={styles.homeTestimonialsContent}>
         <h2 className={styles.headerTitle}>the results speak for themselves</h2>
 
-        <Link href="/testimonials">
-          <CustomButton color="blue" size="medium">
+        <Link href='/testimonials'>
+          <CustomButton color='blue' size='medium'>
             explore all testimonials
           </CustomButton>
         </Link>
@@ -35,13 +36,19 @@ function HomeTestimonials({ testimonials }: HomeTestimonialsProps) {
         }}
         grabCursor
         loop
-        slidesPerView="auto"
-        className="!max-w-full !w-full !pl-4 2xl:!pl-[calc((100vw-1503px)/2)]"
+        slidesPerView='auto'
+        autoplay={{
+          delay: 100,
+          disableOnInteraction: true,
+        }}
+        speed={10000}
+        modules={[Autoplay]}
+        className='!max-w-full !w-full !pl-4 2xl:!pl-[calc((100vw-1503px)/2)] logo-slider'
       >
         {[...testimonials, ...testimonials].map((testimonial, index) => (
           <SwiperSlide
             key={index}
-            className="!max-w-[clamp(320px,40.46vw,437px)] !w-full"
+            className='!max-w-[clamp(320px,40.46vw,437px)] !w-full'
           >
             <div className={styles.testimonialsComponent}>
               <h4 className={styles.testimonialsTitle}>{testimonial.quote}</h4>
@@ -55,7 +62,7 @@ function HomeTestimonials({ testimonials }: HomeTestimonialsProps) {
                 <div className={styles.starContainer}>
                   {Array.from({ length: 5 }).map((_, index) => (
                     <span key={index}>
-                      <FaStar color="#2de046" size={18} />
+                      <FaStar color='#2de046' size={18} />
                     </span>
                   ))}
                 </div>
