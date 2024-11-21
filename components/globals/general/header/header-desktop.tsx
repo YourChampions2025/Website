9b52bdcb023317b47c5863a482911a1b0958c677
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-11-21 11:12:44 -0300

    feat:resolving comments to improve the application

--- components/globals/general/header/header-desktop.tsx
+++ components/globals/general/header/header-desktop.tsx
@@ -1,99 +1,98 @@
-import React from 'react';
-import style from './header.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
-import CustomButton from '../../forms/custom-button/custom-button';
-import { IoLogoFacebook } from 'react-icons/io5';
-import { AiFillInstagram, AiFillTikTok, AiFillYoutube } from 'react-icons/ai';
-import { FaGoogle, FaLinkedin } from 'react-icons/fa';
-import { MdMailOutline, MdPhone } from 'react-icons/md';
-import fischerRedavidLogo from '@/public/images/fischer-redavid-trial-lawyers-logo.svg';
-import { Menu } from '@headlessui/react';
+import React from "react";
+import style from "./header.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import CustomButton from "../../forms/custom-button/custom-button";
+import { IoLogoFacebook } from "react-icons/io5";
+import { AiFillInstagram, AiFillTikTok, AiFillYoutube } from "react-icons/ai";
+import { FaGoogle, FaLinkedin } from "react-icons/fa";
+import { MdMailOutline, MdPhone } from "react-icons/md";
+import fischerRedavidLogo from "@/public/images/fischer-redavid-trial-lawyers-logo.svg";
+import { Menu } from "@headlessui/react";
 
-import { GoTriangleDown } from 'react-icons/go';
+import { GoTriangleDown } from "react-icons/go";
+import { FaXTwitter } from "react-icons/fa6";
 
 const socialLinks = [
   {
-    href: '/',
+    href: "https://www.facebook.com/FischerRedavid/",
     icon: <IoLogoFacebook size={24} />,
   },
   {
-    href: '/',
+    href: "https://www.instagram.com/your_champions?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
     icon: <AiFillInstagram size={24} />,
   },
   {
-    href: '/',
+    href: "https://www.linkedin.com/company/yourchampions/about/",
     icon: <FaLinkedin size={24} />,
   },
   {
-    href: '/',
-    icon: <AiFillTikTok size={24} />,
+    href: "https://x.com/OnJusticePod",
+    icon: <FaXTwitter size={24} />,
   },
   {
-    href: '/',
+    href: "https://www.youtube.com/@onjusticepodcast",
     icon: <AiFillYoutube size={24} />,
   },
   {
-    href: '/',
-    icon: <FaGoogle size={21} />,
+    href: "/",
+    icon: <FaGoogle size={24} />,
   },
 ];
 
 const navbarLinks = [
   {
-    href: '/',
-    label: 'home',
+    href: "/",
+    label: "home",
   },
   {
-    href: '/about-us',
-    label: 'about us',
+    href: "/about-us",
+    label: "about us",
   },
   {
-    href: '/practice-areas',
-    label: 'practice areas',
+    href: "/practice-areas",
+    label: "practice areas",
   },
   {
-    href: '/resources',
-    label: 'resources',
+    href: "/resources",
+    label: "resources",
   },
   {
-    href: '/locations',
-    label: 'locations',
+    href: "/locations",
+    label: "locations",
   },
   {
-    href: '/results',
-    label: 'results',
+    href: "/results",
+    label: "results",
   },
 ];
 
 const contacts = [
   {
-    icon: <MdPhone size={14} color='#2de046' />,
-    textContact: '(888) 694-0708',
-    href: 'tel:8886940708',
+    icon: <MdPhone size={14} color="#2de046" />,
+    textContact: "(888) 694-0708",
+    href: "tel:8886940708",
   },
   {
-    icon: <MdMailOutline size={14} color='#2de046' />,
-    textContact: 'contact@frtriallawyers.com',
-    href: 'mailto:contact@frtriallawyers.com',
+    icon: <MdMailOutline size={14} color="#2de046" />,
+    textContact: "contact@frtriallawyers.com",
+    href: "mailto:contact@frtriallawyers.com",
   },
 ];
 
-function HeaderDesktop() {
-  const handleScrollToContactUs = () => {
-    const contactUsSection = document.getElementById('contact-us');
-    if (contactUsSection) {
-      contactUsSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+interface MenuItem {
+  href: string;
+  label: string;
+}
 
+function HeaderDesktop() {
   return (
     <>
       <div className={style.headerFirstRow}>
-        <Link href='/'>
+        <Link href="/">
           <Image
             src={fischerRedavidLogo}
-            alt='Fischer & Redavid Trial Lawyers Logo'
+            alt="Fischer & Redavid Trial Lawyers Logo"
             width={200}
           />
         </Link>
@@ -101,83 +100,108 @@ function HeaderDesktop() {
         <div className={style.socialLinks}>
           {socialLinks.map((data, i) => (
             <Link
-              className='hover:text-green-500 transition-all duration-300'
+              className="hover:text-green-500 transition-all duration-300"
               key={i}
               href={data.href}
-              target='_blank'
+              target="_blank"
             >
               {data.icon}
             </Link>
           ))}
-          <CustomButton size='small' onClick={handleScrollToContactUs}>
-            ONLINE INTAKE FORM
-          </CustomButton>
+          <Link href="#contact-us">
+            <CustomButton size="small">ONLINE INTAKE FORM</CustomButton>
+          </Link>
         </div>
       </div>
 
       <div className={style.headerSecondRow}>
         <div className={style.navigationLinks}>
-          {navbarLinks.map((data, i) =>
-            data.href === '/resources' ? (
-              <Menu
-                key={i}
-                as='div'
-                className='relative inline-block leading-3'
-              >
-                <Menu.Button className={style.menuButton}>
-                  {data.label} <GoTriangleDown />
-                </Menu.Button>
-                <Menu.Items className={style.menuItems}>
-                  <Menu.Item>
-                    {() => (
-                      <Link href='/terrys-takes' className={style.menuItem}>
-                        Terry's Takes{' '}
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {() => (
-                      <Link href='/careers' className={style.menuItem}>
-                        Careers
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {() => (
-                      <Link href='/podcasts' className={style.menuItem}>
-                        Podcast
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {() => (
-                      <Link href='/articles' className={style.menuItem}>
-                        Articles
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {() => (
-                      <Link href='/video-center' className={style.menuItem}>
-                        Video Center{' '}
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {() => (
-                      <Link href='/testimonials' className={style.menuItem}>
-                        Testimonials
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </Menu.Items>
-              </Menu>
-            ) : (
+          {navbarLinks.map((data, i) => {
+            const renderMenuItems = (items: MenuItem[]) =>
+              items.map((item, index) => (
+                <Menu.Item key={index}>
+                  {() => (
+                    <Link href={item.href} className={style.menuItem}>
+                      {item.label}
+                    </Link>
+                  )}
+                </Menu.Item>
+              ));
+
+            if (data.href === "/practice-areas") {
+              return (
+                <Menu
+                  key={i}
+                  as="div"
+                  className="relative inline-block leading-3"
+                >
+                  <Menu.Button className={style.menuButton}>
+                    {data.label} <GoTriangleDown />
+                  </Menu.Button>
+                  <Menu.Items className={style.menuItems}>
+                    {renderMenuItems([
+                      { href: "/practice-areas", label: "all practice areas" },
+                      {
+                        href: "/practice-areas/catastrophic-injuries",
+                        label: "Catastrophic Injuries",
+                      },
+                      {
+                        href: "/practice-areas/burn-injuries",
+                        label: "Burn Injuries",
+                      },
+                      { href: "/practice-areas/drowning", label: "Drowning" },
+                      {
+                        href: "/practice-areas/traumatic-brain",
+                        label: "Traumatic Brain Injuries",
+                      },
+                      {
+                        href: "/practice-areas/wrongful-death",
+                        label: "Wrongful Death",
+                      },
+                      {
+                        href: "/practice-areas/medical-malpractice",
+                        label: "Medical Malpractice",
+                      },
+                      {
+                        href: "/practice-areas/product-liability",
+                        label: "Product Liability",
+                      },
+                    ])}
+                  </Menu.Items>
+                </Menu>
+              );
+            }
+
+            if (data.href === "/resources") {
+              return (
+                <Menu
+                  key={i}
+                  as="div"
+                  className="relative inline-block leading-3"
+                >
+                  <Menu.Button className={style.menuButton}>
+                    {data.label} <GoTriangleDown />
+                  </Menu.Button>
+                  <Menu.Items className={style.menuItems}>
+                    {renderMenuItems([
+                      { href: "/terrys-takes", label: "Terry's Takes" },
+                      { href: "/careers", label: "Careers" },
+                      { href: "/podcasts", label: "Podcast" },
+                      { href: "/articles", label: "Articles" },
+                      { href: "/video-center", label: "Video Center" },
+                      { href: "/testimonials", label: "Testimonials" },
+                    ])}
+                  </Menu.Items>
+                </Menu>
+              );
+            }
+
+            return (
               <Link key={i} href={data.href} className={style.link}>
                 {data.label}
               </Link>
-            )
-          )}
+            );
+          })}
         </div>
 
         <div className={style.contactLinks}>
