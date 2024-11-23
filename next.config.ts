513cbc2c97cf28eda198acde8e33a5b5c62e06b2
Pedro Palhari <pedropalhari@gmail.com>
2024-11-22 21:24:30 -0300

    feat: more redirects

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,94 @@ const nextConfig: NextConfig = {
         destination: "/podcasts",
         permanent: true,
       },
+      {
+        source: "/site-map",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/our-team/jordan-redavid",
+        destination: "/about-us/our-attorneys/jordan-redavid-esq-",
+        permanent: true,
+      },
+      {
+        source: "/miami-truck-accident-lawyer",
+        destination: "/miami/truck-accident-lawyer",
+        permanent: true,
+      },
+      {
+        source: "/our-team/jordan-redavid-partner",
+        destination: "/about-us/our-attorneys/jordan-redavid-esq-",
+        permanent: true,
+      },
+      {
+        source: "/personal-injury",
+        destination: "/hollywood-fl",
+        permanent: true,
+      },
+      {
+        source: "/reviews",
+        destination: "/testimonials",
+        permanent: true,
+      },
+      {
+        source: "/our-team",
+        destination: "/about-us",
+        permanent: true,
+      },
+      {
+        source: "/jordan-redavid",
+        destination: "/about-us/our-attorneys/jordan-redavid-esq-",
+        permanent: true,
+      },
+      {
+        source: "/our-team/john-fischer",
+        destination: "/about-us/our-attorneys/john-fischer-esq-",
+        permanent: true,
+      },
+      {
+        source: "/miami-medical-malpractice-lawyer",
+        destination: "/miami/medical-malpractice-lawyer",
+        permanent: true,
+      },
+      {
+        source: "/personal-injury/medical-malpractice",
+        destination: "/gainesville/medical-malpractice-lawyer",
+        permanent: true,
+      },
+      {
+        source: "/miami-motorcycle-accident-lawyer",
+        destination: "/miami/motorcycle-accident-lawyer",
+        permanent: true,
+      },
+      {
+        source: "/is-wrong-diagnosis-considered-medical-malpractice",
+        destination:
+          "/blog/2019/january/is-wrong-diagnosis-considered-medical-malpractic",
+        permanent: true,
+      },
+      {
+        source: "/about-us/our-attorneys/john-fischer",
+        destination: "/about-us/our-attorneys/john-fischer-esq-",
+        permanent: true,
+      },
+      {
+        source: "/can-i-sue-my-landlord-for-negligent-security",
+        destination:
+          "/blog/2018/december/can-i-sue-my-landlord-for-negligent-security-",
+        permanent: true,
+      },
+      {
+        source: "/hollywood-personal-injury-lawyer",
+        destination: "/hollywood-fl",
+        permanent: true,
+      },
+      {
+        source: "/how-much-compensation-is-my-car-accident-worth",
+        destination:
+          "/blog/2017/july/how-much-compensation-is-my-car-accident-worth-",
+        permanent: true,
+      },
     ];
   },
 };
