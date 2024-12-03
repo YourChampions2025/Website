02eb5f46b6c45ebf8788b2f16fc8439913d060af
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-12-02 21:35:37 -0300

    fix: update faq

--- components/screens/home/frequent-questions/frequent-questions.tsx
+++ components/screens/home/frequent-questions/frequent-questions.tsx
@@ -15,19 +15,14 @@ const questionsAndAnswers = [
       "You will file an insurance claim with your own auto insurance company after your car accident. Florida is a fault state for car accidents, so you are required to carry personal injury protection (PIP) coverage as part of your auto insurance policy. This coverage means your insurer has an obligation to cover certain losses when you are involved in a collision. But, insurers are rarely fair when it comes to victim compensation.",
   },
   {
-    question: "How much is my claim worth?",
+    question: "What Should I Do After an Accident?",
     answer:
-      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+      "After an accident, stay calm and move to a safe location to avoid further risks. Check for injuries and assess the damage. If the other driver is reliable, exchange basic information such as name, driver’s license details, and insurance information. Gather evidence immediately by taking photos of the scene, injuries, and all vehicles involved.",
   },
   {
-    question: "Do I need to hire a personal injury lawyer?",
+    question: "Can I sue for a slip-and-fall injury if I was partly at fault?",
     answer:
-      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-  },
-  {
-    question: "How much does a personal injury lawyer cost?",
-    answer:
-      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+      "You can still file a lawsuit even if you were partially at fault. According to the state’s comparative negligence laws, victims who are somehow responsible for their injuries can receive compensation that’s less than the percentage of fault contributed. Even so, your lawyer will still focus on maximizing the settlement you’re due. ",
   },
 ];
 
