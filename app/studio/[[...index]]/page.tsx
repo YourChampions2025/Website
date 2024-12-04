a114f415ae2f3eb15783d056ff127f16cf2cd133
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-12-03 21:05:29 -0300

    feat: sanity header adjustment

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,7 +1,23 @@
 "use client";
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity.config";
+import Image from "next/image";
+import Logo from "@/public/images/fischer-and-redavid-sanity-logo.png";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <div className="relative">
+      <div className="w-fit h-[49px] flex items-center justify-center bg-[#13141B] pr-5 fixed left-10 md:left-3 top-0 z-50">
+        <Image
+          className="max-w-[158px] w-full h-fit object-contain"
+          src={Logo}
+          alt="Fischer And Redavid Trial Lawyers"
+        />
+      </div>
+
+      <div className="relative z-10">
+        <NextStudio config={config} />
+      </div>
+    </div>
+  );
 }
